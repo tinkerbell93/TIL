04d720073e2101574e8272571d499e9757a02dd4
tinkerbell93 <hhjj105@gmail.com>
2020-06-14 19:06:21 +0900

    feat: coding v3.0 TODO

--- JavaScript/FDS/TODO/v4.0/public/js/app.js
+++ JavaScript/FDS/TODO/v4.0/public/js/app.js
@@ -1,165 +1,125 @@
-// import { ajax } from './ajax.js';
-
 // State
 let todos = [];
 let navState = 'all';
 
 // DOMs
 const $todos = document.querySelector('.todos');
-const $inputTodo = document.querySelector('.input-todo');
-const $completeAll = document.querySelector('.complete-all > .checkbox');
-const $completedTodos = document.querySelector('.completed-todos');
-const $activeTodos = document.querySelector('.active-todos');
-const $clearCompleted = document.querySelector('.clear-completed > .btn');
 const $nav = document.querySelector('.nav');
+const $input = document.querySelector('.input-todo');
+const $completedAll = document.getElementById('ck-complete-all');
+const $completedtodos = document.querySelector('.completed-todos');
+const $activeTodos = document.querySelector('.active-todos');
+const $clearCompleted = document.querySelector('.clear-completed .btn');
 
+// FUNCTIONs
+const checkCompletedAll = () => {
+  todos.every(todo => todo.completed) && todos.length ? $completedAll.checked = true : $completedAll.checked = false;
+};
+ 
 const render = () => {
-  let html = '';
-  const _todos = todos.filter(({ completed }) => (navState === 'completed' ? completed : navState === 'active' ? !completed : true));
+  const _todos = todos.filter(todo => (navState === 'all' ? todo : (navState === 'active' ? !todo.completed : todo.completed)));
 
+  let html = '';
   _todos.forEach(({ id, content, completed }) => {
     html += `<li id="${id}" class="todo-item">
-        <input id="ck-${id}" class="checkbox" type="checkbox" ${completed ? 'checked' : ''}>
-        <label for="ck-${id}">${content}</label>
-        <i class="remove-todo far fa-times-circle"></i>
-      </li>`;
-    console.log(content);
+    <input id="ck-${id}" class="checkbox" type="checkbox" ${completed ? 'checked' : ''}>
+    <label for="ck-${id}">${content}</label>
+    <i class="remove-todo far fa-times-circle"></i>
+  </li>`;
   });
 
   $todos.innerHTML = html;
-  $completedTodos.textContent = todos.filter(({ completed }) => completed).length;
-  $activeTodos.textContent = todos.filter(({ completed }) => !completed).length;
+  $completedtodos.textContent = completedTodos();
+  $activeTodos.textContent = todos.length - completedTodos();
+  checkCompletedAll();
 };
-
-// Ajax
-const ajax = (() => {
-  const req = (method, url, callback, payload) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.send(JSON.stringify(payload));
-
-    xhr.onload = () => {
-      if (xhr.status === 200 || xhr.status === 201) {
-        callback(JSON.parse(xhr.response));
-      } else {
-        console.error(xhr.status);
-      }
-    };
-  };
-
-  return {
-    get(url, callback) {
-      req('GET', url, callback);
-    },
-    post(url, payload, callback) {
-      req('POST', url, callback, payload);
-    },
-    patch(url, payload, callback) {
-      req('PATCH', url, callback, payload);
-    },
-    delete(url, callback) {
-      req('DELETE', url, callback);
-    }
-  };
-})();
 
 const getTodos = () => {
-  ajax.get('/todos', _todos => {
-    todos = _todos;
-    render();
-    // console.log('[getTodos]', todos);
-  });
+  todos = [
+    { id: 1, content: 'HTML', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 3, content: 'Javascript', completed: false }
+  ].sort((todo1, todo2) => todo2.id - todo1.id);
+
+  render();
 };
 
-const generateId = () => (todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1);
+const changeNavState = id => {
+  [...$nav.children].forEach(navItem => {
+    navItem.classList.toggle('active', id === navItem.id);
+  });
+
+  navState = id;
+  render();
+};
+
+const generate = () => Math.max(0, ...todos.map(todo => todo.id)) + 1;
 
 const addTodo = content => {
-  const newTodo = { id: generateId(), content, completed: false };
-  ajax.post('/todos', newTodo, _todos => {
-    todos = _todos;
-    render();
-  });
-  console.log('[addTodo]', todos);
+  todos = [{ id: generate(), content, completed: false }, ...todos];
+  render();
 };
 
-const toggleTodo = (id, completed) => {
-  ajax.patch(`/todos/${id}`, { completed }, _todos => {
-    todos = _todos;
-    render();
-  });
-  console.log('[toggleTodo]', todos);
+const completedTodo = id => {
+  todos = todos.map(todo => (todo.id === +id ? { ...todo, completed: !todo.completed } : todo));
+  render();
 };
 
 const removeTodo = id => {
   todos = todos.filter(todo => todo.id !== +id);
-  ajax.delete(`/todos/${id}`, () => {
-    todos = todos.filter(todo => todo.id !== +id);
-    render();
-  });
-  console.log('[removeTodo]', todos);
-};
 
-const toggleCompleteAll = completed => {
-  ajax.patch('/todos', { completed }, _todos => {
-    todos = _todos;
-    render();
-  });
-  console.log('[toggleCompleteAll]', todos);
-};
-
-const removeCompleted = () => {
-  ajax.delete('/todos/completed', _todos => {
-    todos = _todos;
-    render();
-  });
-  // todos = todos.filter(todo => !todo.completed);
-  console.log('[removeCompleted]', todos);
-
-};
-
-const changeNavState = id => {
-  [...$nav.children].forEach($navItem => {
-    $navItem.classList.toggle('active', $navItem.id === id);
-  });
-
-  navState = id;
-
-  console.log('[changeNavState]', navState);
   render();
 };
 
-// Event bindings
+const completedAll = completed => {
+  todos = todos.map(todo => ({ ...todo, completed }));
+
+  render();
+};
+
+const completedTodos = () => todos.filter(todo => todo.completed).length;
+
+const clearCompleted = () => {
+  todos = todos.filter(todo => !todo.completed);
+
+  render();
+};
+
+// EVENTs
 window.onload = getTodos;
 
-$inputTodo.onkeyup = ({ target, keyCode }) => {
-  const content = target.value.trim();
-  if (keyCode !== 13 || content === '') return;
-  console.log(content);
+// NAV
+$nav.onclick = ({ target }) => {
+  if (!target.matches('li')) return;
+  // document.querySelector('.active').classList.remove('active');
+  // target.classList.add('active');
+  changeNavState(target.id);
+};
 
+// ADD TODO
+$input.onkeyup = ({ keyCode, target }) => {
+  if (keyCode !== 13) return;
   addTodo(target.value);
   target.value = '';
 };
 
-$todos.onchange = e => {
-  toggleTodo(e.target.parentNode.id, e.target.checked);
+// COMPLELTED TODO
+$todos.onchange = ({ target }) => {
+  completedTodo(target.parentNode.id);
 };
 
-$todos.onclick = e => {
-  if (!e.target.matches('.todos > li > .remove-todo')) return;
-  removeTodo(+e.target.parentNode.id);
+// REMOVE TODO
+$todos.onclick = ({ target }) => {
+  if (!target.matches('.todo-item i')) return;
+  removeTodo(target.parentNode.id);
 };
 
-$completeAll.onchange = e => {
-  toggleCompleteAll(e.target.checked);
+// COMPLETED ALL
+$completedAll.onchange = ({ target }) => {
+  completedAll(target.checked);
 };
 
+// ClEAR COMPLETED
 $clearCompleted.onclick = () => {
-  // console.loge(e.target.id);
-  removeCompleted();
-};
-
-$nav.onclick = ({ target }) => {
-  if (!target.matches('.nav > li:not(.active)')) return;
-  changeNavState(target.id);
-};
+  clearCompleted();
+}
