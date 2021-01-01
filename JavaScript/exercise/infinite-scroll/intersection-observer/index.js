2aa2832d77eef0787c566084436b0948cd1d8297
tinkerbell93 <hhjj105@gmail.com>
2021-01-01 19:54:37 +0900

    TIL: infinite scroll - scroll event

--- JavaScript/exercise/infinite-scroll/intersection-observer/index.js
+++ JavaScript/exercise/infinite-scroll/intersection-observer/index.js
@@ -1,32 +1,46 @@
+// vanila - scroll event
 (() => {
-  let ioTarget;
-  const $ul = document.querySelector('ul')
+  const $ul = document.querySelector('ul');
   let $li = document.querySelector('li:last-child');
   let count = $ul.children.length;
 
-  // 2. 감시 기능 선언
-  const io = new IntersectionObserver((entry, observer) => {
-    // 현재 보이는 target 출력
-    ioTarget = entry[0].target;
-
-    // 현재 보이는 target이 출력되면 하는 일
-    if (entry[0].isIntersecting) {
-      console.log('현재 보이는 타켓', ioTarget)
-      // 현재 보이는 target 감시 취소해줘
-      io.unobserve($li);
-
-      // 새로운 li 추가해
+  document.addEventListener('scroll', () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       $li = $ul.appendChild(document.createElement('li'));
       $li.textContent = ++count;
-      // 새로 추가된 li 감시해!
-      io.observe($li);
     }
-  }, {
-    // 타겟이 50% 이상 보이면 해줘!
-    threshold: 0.5
-  });
-
-  // 3. li감시해!
-  io.observe($li);
-
+  })
 })();
+
+
+// vanila - intersection observer
+// (() => {
+//   const $ul = document.querySelector('ul');
+//   let $li = document.querySelector('li:last-child');
+//   let count = $ul.children.length;
+
+//   // 감시 기능 선언
+//   const io = new IntersectionObserver((entry, observer) => {
+//     // 현재 보이는 target 출력
+//     const ioTarget = entry[0].target;
+
+//     // 현재 보이는 target이 출력되면 하는 일
+//     if (entry[0].isIntersecting) {
+//       console.log('현재 보이는 타켓', ioTarget)
+//       // 현재 보이는 target 감시 취소해줘
+//       io.unobserve($li);
+
+//       // 새로운 li 추가해
+//       $li = $ul.appendChild(document.createElement('li'));
+//       $li.textContent = ++count;
+//       // 새로 추가된 li 감시해!
+//       io.observe($li);
+//     }
+//   }, {
+//     // 타겟이 50% 이상 보이면 해줘!
+//     threshold: 0.5
+//   });
+
+//   // li감시해!
+//   io.observe($li);
+// })();
