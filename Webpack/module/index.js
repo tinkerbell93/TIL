47a1306c0750a3a6e78691c2f484c67eca3a491c
tinkerbell93 <hhjj105@gmail.com>
2021-01-02 20:53:58 +0900

    TIL: ESM

--- Webpack/module/index.js
+++ Webpack/module/index.js
@@ -2,8 +2,11 @@
 // 2. PI정의
 // 3. 공식을 통해 결과 얻기
 
-// module 가져오기
-const { getCircleArea } = require('./mathUtil');
+// module 가져오기: CommonJS
+// const { getCircleArea } = require('./mathUtil');
+
+// module 가져오기: ESM
+import { getCircleArea } from './mathUtil';
 
 const result = getCircleArea(2);
 console.log(result);
--- Webpack/module/mathUtil.js
+++ Webpack/module/mathUtil.js
@@ -1,7 +1,7 @@
 const PI = 3.14;
 const getCircleArea = r => r * r * PI;
 
-// module 키워드로 내보내기
+// module 키워드로 내보내기: CommonJS
 // 다른 파일에서 PI, getCircleArea에 접근 할 수 있음
 // module.exports = {
 //   PI,
@@ -9,5 +9,11 @@ const getCircleArea = r => r * r * PI;
 // }
 
 // exports 키워드로 내보내기
-exports.PI = PI;
-exports.getCircleArea = getCircleArea;
+// exports.PI = PI;
+// exports.getCircleArea = getCircleArea;
+
+// module 키워드로 내보내기: ESM
+export {
+  PI,
+  getCircleArea
+}
